634a0de9ca35bc01c1d2b9c5045cdc08ac625f49
Darren Barklie <darrenbarklie@gmail.com>
2017-12-15 22:55:18 +0000

    Switches

---
+++ 12-switches.js
@@ -0,0 +1,43 @@
+// Switches
+const color = 'yellow';
+
+switch(color){
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('Color is not red or blue');
+    break;
+}
+// Same as creating if else conditional, just different syntax
+// Switch is cleaner syntax if many conditions exist are stacked
+
+let day;
+
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+    break;
+}
+console.log(`Today is ${day}`);
