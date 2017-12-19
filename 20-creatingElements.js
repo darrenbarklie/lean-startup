55b061c87cb3f7e701e7025d0165dc5951cc4c55
Darren Barklie <darrenbarklie@gmail.com>
2017-12-19 00:20:19 +0000

    Creating elements

---
+++ 20-creatingElements.js
@@ -0,0 +1,16 @@
+// Create the element 
+const li = document.createElement('li');
+// Add a class 
+li.className = "item added-item";
+// Add an id
+li.id = "added";
+// Add attribute
+li.setAttribute('title', 'New item');
+// Create text node and append
+li.appendChild(document.createTextNode('Hello world'));
+// Append li as child to ul
+document.querySelector('ul.list').appendChild(li);
+
+console.log(li);
+
+
