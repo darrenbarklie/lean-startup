3e71943e577f9ed8812c43e839195bfbfeac6041
Darren Barklie <darrenbarklie@gmail.com>
2018-03-09 21:19:27 +0000

    Setup project 3

---
+++ pjt03-numberGuesser/app.js
@@ -0,0 +1,9 @@
+/*
+- Player must guess a number btween a min and max
+- Play gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify play of a correct guess
+- Notify player of the correct answer if lose
+- Let player choose to play again
+*/
+
