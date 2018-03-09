df4f32a133547b5c02ac5dab2ffb3ad399d3733a
Darren Barklie <darrenbarklie@gmail.com>
2018-03-09 21:40:19 +0000

    Setup variables

--- pjt03-numberGuesser/app.js
+++ pjt03-numberGuesser/app.js
@@ -7,3 +7,21 @@
 - Let player choose to play again
 */
 
+// Game values
+let min = 1, 
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+    
+// UI elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+      
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+      
