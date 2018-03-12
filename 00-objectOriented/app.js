b376fa7efd92d586e83d69e9c28120749bed4bd3
Darren Barklie <darrenbarklie@gmail.com>
2018-03-12 00:16:15 +0000

    Object constructors

--- 00-objectOriented/app.js
+++ 00-objectOriented/app.js
@@ -44,3 +44,37 @@ function Person(name, dob) {
 
 const dazzle = new Person('Daz', '8-22-1985');
 console.log(dazzle.calculateAge());
+
+
+
+// OBJECT CONSTRUCTORS
+// Typically avoided as confusing and not performant
+// but good knowledge in case encountered in the wild
+
+// String
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+// Note: name2 typeof is obect, not string
+// Number
+const num1 = 5;
+const num2 = new Number(5);
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
+// Function
+const getSum1 = function(x,y){
+  return x + y;
+}
+const getSum2 = new Function ('x', 'y', 'return 1 + 1');
+// Object 
+const john1 = {name: "John"};
+const john2 = new Object({name: "John"});
+// Arrays
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('//\w+');
+
+
+
