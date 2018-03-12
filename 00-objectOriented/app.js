93bcec667f6d0628e0fe5da977d2dad2978eee4c
Darren Barklie <darrenbarklie@gmail.com>
2018-03-12 00:41:06 +0000

    Prototype functions, Object prototypes

--- 00-objectOriented/app.js
+++ 00-objectOriented/app.js
@@ -98,16 +98,25 @@ Person.prototype.calculateAge = function(){
   const ageDate = new Date(diff);
   return Math.abs(ageDate.getUTCFullYear() - 1970); 
 }
-// Get 
-
+// Get full name 
+Person.prototype.getFullName = function(){
+  return `${this.firstName} ${this.lastName}`;
+}
+// Gets married
+Person.prototype.getsMarried = function(newLastName){
+  this.lastName = newLastName;
+}
 
 const john = new Person('Jonh', 'Wick', '04-01-1967');
 const mary = new Person('Mary', 'Jane', 'March 20 1978');
 console.log(mary);
 
 console.log(john.calculateAge());
+console.log(john.getFullName());
+console.log(mary.getFullName());
+mary.getsMarried('Smith');
+console.log(mary.getFullName());
 
-
-
+console.log(mary.hasOwnProperty('firstName')); // true
 
  
